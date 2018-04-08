c8ef972e426ca1cca3964723b6bd90e2e6662930
LuckOfWise <luck.of.wise@gmail.com>
2018-04-08 22:15:13 +0900

    キャッシュ周りを実装

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-v1';
+const CACHE_NAME = 'cache-v2';
 const CACHE_URLS = [
   '/',
   '/?homescreen=1',
@@ -18,14 +18,40 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      self.clients.claim();
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        return response || fetch(event.request);
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
       })
     );
 });
