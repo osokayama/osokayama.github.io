f97728259c8c4cb5ead1184a72a72a0d80546598
Makoto Henmi <mako.mail.g@gmail.com>
2019-05-09 21:15:44 +0900

    スポンサーのスマホ表示崩れを修正

--- src/js/components/sponsor.vue
+++ src/js/components/sponsor.vue
@@ -6,72 +6,72 @@
         <div class="siimple-box-title"><i class="far fa-flag"/> 協賛企業・協力・後援団体一覧</div>
       </div>
       <div class="sponsor">
-        <h2>協賛企業（50音順・敬称略）</h2>
-        <h3>プラチナスポンサー</h3>
-        <div class="siimple-grid-row platina">
-          <div
+        <h2 class="siimple-h5">協賛企業（50音順・敬称略）</h2>
+        <h3 class="siimple-h4">プラチナスポンサー</h3>
+        <ul class="sponsor-list platina">
+          <li
             v-for="targetSponsor in sponsor.platina"
             :key="targetSponsor.name"
-            class="siimple-grid-col siimple-grid-col--xl-4 siimple-grid-col--md-6 siimple-grid-col--sm-12">
+            class="sponsor-list-item">
             <a :href="targetSponsor.url">
               <img
                 :src="targetSponsor.image"
                 :alt="targetSponsor.name">
             </a>
-          </div>
-        </div>
-        <h3>ゴールドスポンサー</h3>
-        <div class="siimple-grid-row gold">
-          <div
+          </li>
+        </ul>
+        <h3 class="siimple-h4">ゴールドスポンサー</h3>
+        <ul class="sponsor-list gold">
+          <li
             v-for="targetSponsor in sponsor.gold"
             :key="targetSponsor.name"
-            class="siimple-grid-col siimple-grid-col--xl-3 siimple-grid-col--md-4 siimple-grid-col--sm-12">
+            class="sponsor-list-item">
             <a :href="targetSponsor.url">
               <img
                 :src="targetSponsor.image"
                 :alt="targetSponsor.name">
             </a>
-          </div>
-        </div>
-        <h3>シルバースポンサー</h3>
-        <div class="siimple-grid-row silver">
-          <div
+          </li>
+        </ul>
+        <h3 class="siimple-h4">シルバースポンサー</h3>
+        <ul class="sponsor-list silver">
+          <li
             v-for="targetSponsor in sponsor.silver"
             :key="targetSponsor.name"
-            class="siimple-grid-col siimple-grid-col--xl-2 siimple-grid-col--md-4 siimple-grid-col--sm-6">
+            class="sponsor-list-item">
             <a :href="targetSponsor.url">
               <img
                 :src="targetSponsor.image"
                 :alt="targetSponsor.name">
             </a>
-          </div>
-        </div>
-        <h3>ブロンズスポンサー</h3>
-        <div class="siimple-grid-row bronze">
-          <div
+          </li>
+        </ul>
+        <h3 class="siimple-h4">ブロンズスポンサー</h3>
+        <ul class="sponsor-list bronze">
+          <li
             v-for="targetSponsor in sponsor.bronze"
             :key="targetSponsor.name"
-            class="siimple-grid-col siimple-grid-col--xl-2 siimple-grid-col--md-4 siimple-grid-col--sm-6">
+            class="sponsor-list-item">
             <a :href="targetSponsor.url">
               <img
                 :src="targetSponsor.image"
                 :alt="targetSponsor.name">
             </a>
-          </div>
-        </div>
-        <h3>ツール支援</h3>
-        <div class="siimple-grid-row tool">
-          <div
+          </li>
+        </ul>
+        <h3 class="siimple-h4">ツール支援</h3>
+        <ul class="sponsor-list tool">
+          <li
             v-for="targetToolSupporter in tool"
             :key="targetToolSupporter.name"
-            class="siimple-grid-col siimple-grid-col--xl-2 siimple-grid-col--md-4 siimple-grid-col--sm-6">
+            class="sponsor-list-item">
             <a :href="targetToolSupporter.url">
               <img
                 :src="targetToolSupporter.image"
                 :alt="targetToolSupporter.name">
             </a>
-          </div>
-        </div>
+          </li>
+        </ul>
         <h2 v-if="support.length > 0">後援（50音順・敬称略）</h2>
         <ul v-if="support.length > 0">
           <li
@@ -99,12 +99,14 @@ export default {
 </script>
 <style scoped>
 /* sponsor */
+.sponsor-list { text-align: center; list-style: none; padding: 0; margin-bottom: 60px; }
+.sponsor-list-item { display: inline-block; vertical-align: middle; padding: 10px; }
+.sponsor-list-item img { max-width: 100%; }
 .sponsor { text-align: center; margin: 0 10px; }
-.sponsor .siimple-grid-row { display: -webkit-flex; display: flex; -webkit-justify-content: center; justify-content: center; -webkit-align-items: center; align-items: center; }
-.sponsor .platina img {width: 300px; max-height: 200px; object-fit: contain;}
-.sponsor .gold img {width: 229px; max-height: 153px; object-fit: contain;}
+.sponsor .platina img {width: 270px; max-height: 200px; object-fit: contain;}
+.sponsor .gold img {width: 229px; max-height: 100px; object-fit: contain;}
 .sponsor .silver img {width: 200px; max-height: 134px; object-fit: contain;}
-.sponsor .bronze img {width: 150px; max-height: 100px; object-fit: contain;}
+.sponsor .bronze img {width: 170px; max-height: 100px; object-fit: contain;}
 .sponsor .tool img {width: 229px; max-height: 153px; object-fit: contain;}
 .sponsor ul { list-style: none; }
 </style>
