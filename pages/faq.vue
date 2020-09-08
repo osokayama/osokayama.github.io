5c97325ceebfcab64fd1264a8672c3c0c391c4d6
a-know <a.know.dev@gmail.com>
2020-09-08 09:21:36 +0900

    開催方式の変更を受けて、FAQを刷新

--- pages/faq.vue
+++ pages/faq.vue
@@ -5,19 +5,19 @@
       <div class="siimple-box page-title">
         <div class="siimple-box-title"><i class="fas fa-question"/> よくあるご質問</div>
       </div>
-      <h2><i class="fas fa-question"/> 最寄り駅はどこですか？</h2>
+      <h2><i class="fas fa-question"/> どのような開催形式になりますか？</h2>
       <p class="siimple-p">
-        JR倉敷駅となります。今年の会場は倉敷アイビースクエアです。
+        社会情勢を鑑みて、オンライン形式での開催を予定しています。オープンセミナー岡山では初の試みとなります。
       </p>
-      <h2><i class="fas fa-question"/> 会場での飲食は可能ですか？</h2>
-      <p class="siimple-p"><b>ホール内では飲食不可です。</b></p>
-      <h2><i class="fas fa-question"/> 車で行こうと思うのですが駐車場はありますか？</h2>
+      <h2><i class="fas fa-question"/> 懇親会はありますか？</h2>
+      <p class="siimple-p">懇親会はオープンセミナー岡山というコミュニティにとって大事なものであると考えており、本編と同じく、オンライン形式での開催を検討しています。</p>
+      <h2><i class="fas fa-question"/> 「2020」なのに2021年の開催なんですか？</h2>
       <p class="siimple-p">
-        ありますが、混雑することが予測されますので、なるべく公共の交通機関をご利用ください。<br />
-        <a href="https://www.ivysquare.co.jp/access/#parking" target="blank" >アイビースクエア駐車場</a>
+        開催がずれ込み、申し訳ありません。<br />
+        ぎりぎりまで物理的な開催を選択肢にいれていたこと、それにより年末年始に差し掛かってしまったこと、実行委員会内にオンライン形式での開催に関する知見が乏しいこと、などが理由としてはあるのですが、参加者のみなさんに楽しんでもらえるイベントにできるよう、実行委員会一同、鋭意準備を進めております。ご理解いただけますと幸いです。
       </p>
       <h2><i class="fas fa-question"/> 来年もやりますか？</h2>
-      <p class="siimple-p">やります。</p>
+      <p class="siimple-p">開催形式は未定ですが、開催予定です。</p>
     </div>
     <app-footer/>
   </div>
