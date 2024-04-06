312f42737793760a3e5415bf99a223b7695d5185
razon <shizonera@gmail.com>
2024-04-06 18:48:39 +0900

    pathの解決にpath-browserifyを使うよう修正

--- plugins/imagepath.js
+++ plugins/imagepath.js
@@ -1,9 +1,10 @@
+import {posix as pathlib} from 'path-browserify'
+
 export default defineNuxtPlugin(() => {
     return {
         provide: {
             imagePath: (path) => {
                 const runtimeConfig = useRuntimeConfig()
-                const pathlib = require('path')
                 return pathlib.join(runtimeConfig.app.baseURL, 'images', path)
             }
         }
