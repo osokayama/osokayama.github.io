d58822bcc9f8c7789390c97813845f6fdd81af0a
razon <shizonera@gmail.com>
2024-06-09 10:18:29 +0900

    [WIP]スタッフページの実装
    2024/06/09時点での提出分

--- data/header_data.js
+++ data/header_data.js
@@ -4,7 +4,7 @@ export default {
     { text: 'よくあるご質問', url: 'faq' },
     // { text: 'タイムテーブル', url: 'timetable' },
     //{ text: '登壇者', url: 'speakers' },
-    // { text: '実行委員会', url: 'staff' },
+    { text: '実行委員会', url: 'staff' },
     // { text: 'スポンサー', url: 'sponsor' },
     { text: '過去の開催実績', url: 'past' }
   ]
